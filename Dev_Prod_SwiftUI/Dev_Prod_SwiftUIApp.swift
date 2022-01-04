19f338bd01fc68618226febb234df671c38d3ce8
SeongMin-K <eo1tks3@naver.com>
2022-01-04 15:29:05 +0900

    Initial Commit

---
+++ Dev_Prod_SwiftUI/Dev_Prod_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_Prod_SwiftUIApp.swift
+//  Dev_Prod_SwiftUI
+//
+//  Created by SeongMinK on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct Dev_Prod_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
